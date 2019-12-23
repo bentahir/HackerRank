d10fdc093920a208e56d909bad1937ced25dc619
Galaxy book <Galaxy book@ihsane-PC>
2019-12-23 11:57:41 +0100

    first java prtogram in my Galaxy NoteBook

---
+++ Helloworld/src/Helloworld/main.java
@@ -0,0 +1,12 @@
+package Helloworld;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ASSALAMOU ALAYKOUM");
+		
+
+	}
+
+}
