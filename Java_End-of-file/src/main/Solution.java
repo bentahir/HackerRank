cd3b134f12c3561460af3425ddb39bae480b321b
ibentahi <ibentahi@N-5CG5283DWX.nsn-intra.net>
2019-12-29 14:41:46 +0100

    commit

--- Java_End-of-file/src/main/Solution.java
+++ Java_End-of-file/src/main/Solution.java
@@ -6,15 +6,33 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {  
+public class Solution{
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+    static int B;
+    static int H;
+    static Boolean flag ;
+    static Scanner sc ;
+
+        
+    static  {
+        B = 0;
+        flag = false;
         Scanner sc = new Scanner(System.in);
-        int i =1;
-        while (sc.hasNextLine()){
-            System.out.println(i+" "+sc.nextLine());
-            i++;
-        }
+        B = sc.nextInt();
+        H = sc.nextInt();
+        if (B > 0 && H > 0)
+            flag = true;
+        else
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+       
     }
-}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+}//end of class
