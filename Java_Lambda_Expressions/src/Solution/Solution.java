12d387813ee323ec25de9067478bfeca6154b566
ibentahi <ibentahi@N-5CG5283DWX.nsn-intra.net>
2019-12-30 17:41:11 +0100

    commit x

--- Java_Lambda_Expressions/src/Solution/Solution.java
+++ Java_Lambda_Expressions/src/Solution/Solution.java
@@ -5,7 +5,7 @@ import java.util.*;
 interface PerformOperation {
 	boolean check(int a);
 }
-class MyMath {
+class MyMath  {
 	public static boolean checker(PerformOperation p, int num) {
 		return p.check(num);
 	}
